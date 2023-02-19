720176bb016e143e433e928e5921cc899d83c136
cormorecheleo <leocormo@gmail.com>
2023-02-19 19:44:52 +0100

    ajout des types de variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+    // type de variable :
+    // u8 u16 u32 u64 u128
+    // u = unsigned (entier non signé)
+    // u8 (0->256)
+    // place en mémoire pour cette variable
+    // i8 i16 i32 i64 i128
+    // i = integer (entier signé)
+    // usize & isize
     let ma_variable: u8 = 12;
     println!("Ma variable est egale à {}", ma_variable);
 }
